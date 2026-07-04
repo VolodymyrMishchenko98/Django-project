d113b82a3e621ff2a803f211bed4c41d4e4d3d31
Volodymyr Mishchenko <vmishchenko2013@gmail.com>
2026-07-04 14:30:24 +0300

    Fix build file

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-# Render запускає цей файл при кожному деплої
 set -o errexit
 
 pip install -r requirements.txt
+
+cd cinemaarch
 python manage.py collectstatic --no-input
 python manage.py migrate
